38d42188fd2f2fb443dc77ac8284d416243defbb
XuejinE <74973532+XuejinE@users.noreply.github.com>
2020-12-19 22:36:03 +0800

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args) {
-		System.out.println("abcd");
+		System.out.println("a");
 	}
-	// public static void main(String[] args) {
-	// 	System.out.println("HelloWorld");
-	// }
 }
