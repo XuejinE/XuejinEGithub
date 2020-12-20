7e2b3fe27e8353473d5f40794cb978e1fe7bf36b
XuejinE <74973532+XuejinE@users.noreply.github.com>
2020-12-20 17:45:19 +0800

    Add files via upload

--- CreateThread.java
+++ CreateThread.java
@@ -1,14 +1,15 @@
 package lesson1;
 
 public class CreateThread {
+
     public static void main(String[] args) {
-        Runnable r=new Runnable() {//任务描述的可执行类：传入线程对象的构造方法参数
+        Runnable r = new Runnable() {//任务描述的可执行类：传入线程对象的构造方法
             @Override
             public void run() {//线程运行态时，执行
 
             }
         };
-        Thread t=new Thread(r,"子线程A");
+        Thread t = new Thread(r, "子线程A");
         t.start();
 
         //合并的代码
@@ -17,11 +18,11 @@ public class CreateThread {
             public void run() {
 
             }
-        },"子线程B").start();
+        }, "子线程B").start();
 
-        //lambda表达式，runnable只有一个接口方法，可以直接用lambda表达式
-        new Thread(()-> {
-            System.out.println();//和在run（）方法里写一样的效果
-        },"子线程C").start();
+        //runnable只有一个接口方法，可以直接用lambda表达式
+        new Thread(() -> {
+            System.out.println();//和在run()方法写代码一样的效果
+        }, "子线程C").start();
     }
 }
