dffd42f13e85455f7e0e175b44b7292841571966
XuejinE <74973532+XuejinE@users.noreply.github.com>
2021-01-17 20:05:57 +0800

    Add files via upload

--- TestDemo.java
+++ TestDemo.java
@@ -1,70 +1,7 @@
-package javase;
-
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * 1、静态的方法和成员变量：类名.  ->  不依赖对象。
- * 2、局部变量需要先初始化，后使用。
- * 3、普通的成员方法内部，不能定义静态成员变量,但是可以访问
- *
- * User: GAOBO
- * Date: 2020-07-25
- * Time: 8:35
- */
-class Test {
-
-
-
-
-
-   /* public int aMethod(){
-        System.out.println(m);
-        static int i = 0;
-        i++;
-        return i;
-
-    }
-
-    public int func(){
-        static int i;
-    }*/
-
-}
 public class TestDemo {
-
-    private float f=1.0f;
-
-    int m=12;
-
-    static int n=1;
-
-    /*public static void main(String args[]){
-        TestDemo t = new TestDemo();
-        t.f = 1.0;
-        this.n ;
-        System.out.println(TestDemo.n);
-    }*/
-
-   /* String str;
-
-    static boolean Paddy;
-
-    public static void main(String args[]){
-
-        System.out.println(Paddy);
-
-    }*/
-
-
-    public static void main1(String[] args) {
-        /*TestDemo testDemo = new TestDemo();
-        System.out.println(testDemo.str);*/
-       /* // TODO Auto-generated method stub
-        Test test = null;
-        test.hello();*/
-   /*    int a;
-       System.out.println(a);
-       String s ;
-        System.out.println(s);*/
+    //用递归把1234按顺序打印出1 2 3 4
+    public static  void func(int n) {
+        if(n>9)
+        System.out.println("aaa");
     }
 }
