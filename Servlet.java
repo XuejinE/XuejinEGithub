2f75a0f0a76ccb081505913cc6386e43b6ccbbae
XuejinE <74973532+XuejinE@users.noreply.github.com>
2021-03-11 21:32:14 +0800

    Add files via upload

---
+++ Servlet.java
@@ -0,0 +1,12 @@
+package com.moumou.standard;
+
+import java.io.IOException;
+
+public interface Servlet {
+    void init() throws ServletException;
+
+    void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException;
+
+    void destroy();
+}
+
