76d2ded0551da1c74d2b5eae756b11b7ea5434a9
XuejinE <74973532+XuejinE@users.noreply.github.com>
2021-03-13 21:14:22 +0800

    Add files via upload

--- RequestResponseTask.java
+++ RequestResponseTask.java
@@ -1,51 +1,32 @@
-package v1;
+package com.moumou.tomcat;
 
-import java.io.*;
+import com.moumou.standard.Servlet;
+import com.moumou.standard.http.HttpServletRequest;
+import com.moumou.standard.http.HttpServletResponse;
+
+import javax.naming.Context;
 import java.net.Socket;
 
-public class RequestResponseTask implements Runnable {
+public class RequestResponseTask implements Runnable{
     private final Socket socket;
 
-    public RequestResponseTask(Socket socket) {
-        this.socket = socket;
+    public RequestResponseTask(Socket socket){
+        this.socket=socket;
     }
 
     @Override
     public void run() {
-        try {
-            System.out.println("一条 TCP 连接已经建立");
-
-            // 暂时不读取/解析请求
-
-            // 直接写回响应
-            OutputStream outputStream = socket.getOutputStream();
-            Writer writer = new OutputStreamWriter(outputStream, "UTF-8");
-            PrintWriter printWriter = new PrintWriter(writer);
-
-            Thread.sleep(10_000);
-            // 写响应
-            // 写响应行
-            printWriter.printf("HTTP/1.0 200 OK\r\n");
-            // 写响应头
-            printWriter.printf("Content-Type: text/html; charset=utf-8\r\n");
-            // 写入空行，代表响应头结束
-            printWriter.printf("\r\n");
-            // 写响应体，html 内容
-            printWriter.printf("<h1>正常工作了</h1>");
-            // 刷新，把数据写入 TCP 发送缓冲区
-            printWriter.flush();
-        } catch (IOException | InterruptedException exc) {
-            // 因为单次的请求响应周期错误，不应该影响其他请求响应周期
-            // 所以，我们只做打印，不终止进程
-            exc.printStackTrace(System.out);
-        } finally {
-            try {
-                socket.close();
-                System.out.println("一条 TCP 连接已经释放");
-            } catch (IOException exc) {
-                exc.printStackTrace(System.out);
-            }
-        }
+    /*    //解析并得到请求对象
+        HttpServletRequest request=null;
+        //2.实例化一个响应对象
+        HttpServletResponse response=null;
+        //3.根据request.getContextPath()找到哪个Context进行处理
+        Context context=null;
+        //4.根据request.getServletPath()找到Context中哪个HttpServlet进行处理
+        Servlet servlet=null;
+        //5.调用servlet.service(request,response),交给业务处理
+        servlet.service(request,response);
+        //6.根据response对象中的数据，发送HTTP响应
+        sendResponse(response);*/
     }
 }
-
