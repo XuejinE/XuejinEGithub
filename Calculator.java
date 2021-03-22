3eccec815718758090fea432b761f3d59861508b
XuejinE <74973532+XuejinE@users.noreply.github.com>
2021-03-22 23:47:42 +0800

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+/*public class Calculator{
+
+    public static void main(String[] args) {
+
+
+        demo demo = new demo(10,20);
+        demo.setNum1(10);
+        demo.setNum2(20);
+
+        System.out.println(demo.add());
+        System.out.println(demo.sub());
+        System.out.println(demo.mul());
+        System.out.println(demo.dev());
+
+    }
+}*/
