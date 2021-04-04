42bda6584d51a4dc4b8f9451003e8ba49230c154
XuejinE <74973532+XuejinE@users.noreply.github.com>
2021-04-04 22:43:41 +0800

    Add files via upload

---
+++ MinStack.java
@@ -0,0 +1,26 @@
+/*
+package leetcode.min_stack;
+
+public class MinStack {
+    private Deque<Integer>s1;//普通栈
+        private Deque<Integer>s2;//小数栈
+    }
+    public MinStack(){
+        s1=new LinkedList<>();
+        s2=new LinkedList<>();
+    }
+    public void push(int x){
+     s1.push(x);
+     if(s2.isEmpty()){
+         s2.push(x);
+     }else{
+         int t=s2.peek();
+         if(x<s2.peek()){
+
+         }
+    }
+    public void pop(){
+
+    }
+}
+*/
