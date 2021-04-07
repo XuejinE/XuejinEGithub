0c2cad638e529dbb7d0dc6910ceec1dd51df626f
XuejinE <74973532+XuejinE@users.noreply.github.com>
2021-04-07 16:29:11 +0800

    Add files via upload

---
+++ 复习.java
@@ -0,0 +1,58 @@
+package Review;
+import java.awt.print.Book;
+import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+//public class 复习 {
+        /*public static void main(String[] args) {
+            Collection<String> list=new ArrayList<>();
+            System.out.println(list.size());//返回集合中元素的个数
+            System.out.println(list.isEmpty());//判断集合中是否没有任何元素，俗称空集合
+            list.add("我");
+            list.add("爱");
+            list.add("java");
+            System.out.println(list.size());
+            System.out.println(list.isEmpty());
+            Object[] array=list.toArray();//返回一个装有所有集合中元素的数组
+            System.out.println(Arrays.toString(array));
+            for(String s:list){
+                System.out.println(s);
+            }
+            list.remove("爱");//删除
+            for(String s:list){
+                System.out.println(s);
+            }
+            list.clear();//删除集合中所有元素
+            System.out.println(list.size());
+            System.out.println(list.isEmpty());
+
+
+
+
+          }*/
+
+//    private Object[] array;//保存顺序表的元素，即object类型的引用
+//    private int size;//保存顺序表内数据的个数
+//    public void add(Object o){尾插}
+//    public Object get(int index){获取index位置的下标}
+   /* public static void main(String[] args) {
+       复习 books=new 复习();
+        for(int i=0;i<10;i++) {
+            Person person=(Person)books.get(0);
+           // books.add(new Book());//尾插10本书到顺序表
+        }
+        复习 people=new 复习();
+        for(int i=0;i<10;i++) {
+            people.add(new Person());
+        }
+
+
+    }
+
+
+
+   }
+*/
