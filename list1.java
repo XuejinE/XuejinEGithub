52d0ba9b695834d67411818b1d56962ff3669332
XuejinE <74973532+XuejinE@users.noreply.github.com>
2021-04-17 20:05:07 +0800

    Add files via upload

--- list1.java
+++ list1.java
@@ -28,7 +28,65 @@ public class list1 {
         }
     }
 
-    public 
+    public boolean checkIndex(int index){
+    if(index<0 || index>this.getLength()){
+        System.out.println("下标不合法");
+        return false;
+    }
+    return true;
+    }
+    //任意位置插入，第一个数据节点为0 号下标
+    public void addIndex(int index,int data){
+        if(!checkIndex(index)){
+            return;
+        }
+        if(index==0){
+            addFirst(data);
+            return;
+        }
+        if(index==getLength()){
+            addLast(data);
+            return;
+        }
+        Node cur=searchPrev(index);
+        Node node=new Node(data);
+        node.next=cur.next;
+        cur.next=node;
+    }
+    public Node searchPrev(int index){
+        Node cur=this.head;
+        int count=0;
+        while(count<index-1){
+            cur=cur.next;
+            count++;
+        }
+        return cur;
+    }
+    public int getLength(){
+        Node cur=this.head;
+        int count=0;
+        while(cur!=null){
+            cur=cur.next;
+            count++;
+        }
+    return count;
+    }
+
+   public void remove(int key){
+   if(this.head==null) return;
+   if(this.head.data==key){
+       this.head=this.head.next;
+       return;
+   }
+   Node cur=searchPrev(key);
+   if(cur==null){
+       System.out.println("没有你要删除的数字！");
+       return;
+   }
+   Node del=cur.next;
+   cur.next=del.next;
+    }
+
 
     public void disPlay(){
         Node cur=this.head;
