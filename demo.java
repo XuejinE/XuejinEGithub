6c4a5f0ad4b4cdb087204acbb93e43f45a6e86e8
XuejinE <74973532+XuejinE@users.noreply.github.com>
2021-05-16 21:41:06 +0800

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,21 +1,15 @@
-public class demo {
+public class demo{
     public static void main(String[] args) {
         MyLinkedList myLinkedList=new MyLinkedList();
-        myLinkedList.addFirst(1);
+       myLinkedList.addFirst(1);
         myLinkedList.addFirst(2);
         myLinkedList.addFirst(3);
-        myLinkedList.addFirst(3);
-        myLinkedList.addFirst(3);
-
-        myLinkedList.disPlay();
-        System.out.println();
-      /*  myLinkedList.removeAllKey(2);
-        myLinkedList.disPlay();
-Node newHead=myLinkedList.reverseList();
-myLinkedList.disPlay2(newHead);
-Node ret=myLinkedList.FindKthToTail(1);
-        System.out.println(ret.data);*/
-Node newHead=myLinkedList.deleteDuplication(3);
-        myLinkedList.disPlay2(newHead);
+       // myLinkedList.diaPlay();
+        myLinkedList.addLast(4);
+        myLinkedList.addLast(5);
+        myLinkedList.addLast(6);
+        //myLinkedList.diaPlay();
+        myLinkedList.addIndex(2,3);
+        myLinkedList.diaPlay();
     }
 }
