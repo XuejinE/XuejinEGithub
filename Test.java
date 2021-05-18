e2927f53b135f0d16205201b9e30b9a364c2914c
XuejinE <74973532+XuejinE@users.noreply.github.com>
2021-05-18 22:37:45 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,3 +1,11 @@
-public class Test {
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
+public class Test {
+    public static void main(String[] args) {
+        double x=30;
+        double y=40;
+        double result=sqrt(pow(x,2)+pow(y,2));
+        System.out.println(result);
+    }
 }
